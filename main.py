1f72ec2ae3568918241279e7d56797fe53c40253
lucky-DiMa <97959628+lucky-DiMa@users.noreply.github.com>
2022-05-17 10:54:23 +0500

    INIT3

--- main.py
+++ main.py
@@ -3,17 +3,25 @@ from telebot import types
 from create_bot import bot, token
 from flask import Flask, request
 import reg_h
+
 APP_URL = f'https://my-webhook-telegram-bot.herokuapp.com/{token}'
 reg_h.reg_handlers()
 server = Flask(__name__)
+
+
 @server.route(f"/{token}", methods=["POST"])
-def route():
+def updater():
     json_str = request.get_data().decode("utf-8")
     update = types.Update.de_json(json_str)
     bot.process_new_updates([update])
     return "!", 200
 
-if __name__ == '__main__':
+
+@server.route('/')
+def switch_on():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
-    server.run(host="0.0.0.0",  port=int(os.environ.get("PORT", 5000)))
+    return '!', 200
+
+if __name__ == '__main__':
+    server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
