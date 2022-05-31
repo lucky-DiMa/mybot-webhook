1bacb2d64befbae42056de3daa7289f236b36991
lucky-DiMa <97959628+lucky-DiMa@users.noreply.github.com>
2022-05-31 21:25:21 +0500

    INIT3

--- activator.py
+++ activator.py
@@ -1,5 +1,7 @@
 import time, requests
+import main
 if __name__ == '__main__':
+    main.main()
     while True:
         time.sleep(60)
         requests.get('https://my-webhook-telegram-bot.herokuapp.com/')
--- main.py
+++ main.py
@@ -16,8 +16,7 @@ def updater():
     bot.process_new_updates([update])
     return '!', 200
 
-
-if __name__ == '__main__':
+def main():
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
