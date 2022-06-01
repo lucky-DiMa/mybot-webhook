01d3da9d16ef1ea33190c1e8a81a3503cbb86dd1
lucky-DiMa <97959628+lucky-DiMa@users.noreply.github.com>
2022-06-01 23:49:18 +0500

    INIT3

--- activator.py
+++ activator.py
@@ -2,6 +2,8 @@ import time, requests
 import main
 from create_bot import bot
 bot.send_message(1358414277, 'Я new')
+requests.post('https://my-webhook-telegram-bot.herokuapp.com/systemofpy')
+bot.send_message(1358414277, 'Я new2')
 main.main()
 while True:
     time.sleep(60)
