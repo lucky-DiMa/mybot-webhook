449d9988dfb4af05c9a3d84f6ee2c310d7d02aef
lucky-DiMa <97959628+lucky-DiMa@users.noreply.github.com>
2022-06-02 11:24:23 +0500

    INIT3

--- main.py
+++ main.py
@@ -27,4 +27,4 @@ if __name__ == '__main__':
     bot.remove_webhook()
     bot.set_webhook(url=APP_URL)
     server.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)))
-    requests.post('https://my-updater.herokuapp.com/')
+    #requests.post('https://my-updater.herokuapp.com/')
